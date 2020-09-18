fa9290045e5c14c8ed5c5f69f0c8e74e0a423417
Sergey <prodigystd@gmail.com>
2020-09-18 16:29:59 +0500

    base api controller

---
+++ controllers/api/BaseApiController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app\controllers\api;
+
+use yii\rest\Controller;
+use yii\web\NotFoundHttpException;
+
+class BaseApiController extends Controller
+{
+    /**
+     * @param string $errorMessage
+     * @param int $statusCode
+     * @return string[]
+     */
+    public function errorResponse(string $errorMessage, int $statusCode = 500): array
+    {
+        \Yii::$app->response->statusCode = $statusCode;
+        return [
+            'message' => $errorMessage
+        ];
+    }
+
+
+}
