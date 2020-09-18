3668d61ff64cf8ef3829c5cd01b3f25bf9c5cd50
Sergey <prodigystd@gmail.com>
2020-09-18 12:35:28 +0500

    ParserService creation

--- services/ParserService.php
+++ services/ParserService.php
@@ -3,13 +3,39 @@
 namespace app\services;
 
 
+use app\forms\CurrencyForm;
+use yii\helpers\VarDumper;
+use yii\httpclient\Client;
+
 class ParserService
 {
+    const URL = 'http://www.cbr.ru/scripts/XML_daily.asp';
 
-    public function getData(): array
+    /**
+     * @return CurrencyForm[]
+     * @throws \yii\httpclient\Exception
+     */
+    public function getCurrencies(): array
     {
+        $client = new Client();
+        $response = $client->get(self::URL)->send();
 
+        if (!$response->isOk) {
+            return [];
+        }
 
+        $currenciesData = $response->getData()['Valute'];
+
+        $forms = [];
+        foreach ($currenciesData as $currencyData) {
+            $form = new CurrencyForm();
+            $form->name = $currencyData['Name'];
+            $form->rate = $currencyData['Value'];
+            $forms[] = $form;
+
+        }
+
+        return $forms;
     }
 
 }
